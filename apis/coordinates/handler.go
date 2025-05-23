ad7770a0e6232dc42c6a31ecf8f4e0da1a99c640
Carl-Analytix <carl.jordan.marlow@gmail.com>
2025-05-23 11:34:25 -0500

    updated errors

--- apis/coordinates/handler.go
+++ apis/coordinates/handler.go
@@ -1,6 +1,8 @@
 package coordinates
 
 import (
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,7 +11,21 @@ func CartesianToPolarHandler(c *gin.Context) {
 	y := c.Query("y")
 	z := c.Query("z")
 
-	xVal, yVal, zVal := convertToFloat(x, y, z)
+	xVal, err := strconv.ParseFloat(x, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid x value"})
+		return
+	}
+	yVal, err := strconv.ParseFloat(y, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid y value"})
+		return
+	}
+	zVal, err := strconv.ParseFloat(z, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid z value"})
+		return
+	}
 	r, theta, pz := cartesianToPolar(xVal, yVal, zVal)
 
 	c.JSON(200, gin.H{"r": r, "theta": theta, "z": pz})
@@ -20,7 +36,21 @@ func CartesianToSphericalHandler(c *gin.Context) {
 	y := c.Query("y")
 	z := c.Query("z")
 
-	xVal, yVal, zVal := convertToFloat(x, y, z)
+	xVal, err := strconv.ParseFloat(x, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid x value"})
+		return
+	}
+	yVal, err := strconv.ParseFloat(y, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid y value"})
+		return
+	}
+	zVal, err := strconv.ParseFloat(z, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid z value"})
+		return
+	}
 	r, theta, phi := cartesianToSpherical(xVal, yVal, zVal)
 
 	c.JSON(200, gin.H{"r": r, "theta": theta, "phi": phi})
@@ -31,7 +61,21 @@ func PolarToCartesianHandler(c *gin.Context) {
 	theta := c.Query("theta")
 	z := c.Query("z")
 
-	rVal, thetaVal, zVal := convertToFloat(r, theta, z)
+	rVal, err := strconv.ParseFloat(r, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid r value"})
+		return
+	}
+	thetaVal, err := strconv.ParseFloat(theta, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid theta value"})
+		return
+	}
+	zVal, err := strconv.ParseFloat(z, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid z value"})
+		return
+	}
 	cx, cy, cz := polarToCartesian(rVal, thetaVal, zVal)
 
 	c.JSON(200, gin.H{"x": cx, "y": cy, "z": cz})
@@ -42,7 +86,21 @@ func PolarToSphericalHandler(c *gin.Context) {
 	theta := c.Query("theta")
 	z := c.Query("z")
 
-	rVal, thetaVal, zVal := convertToFloat(r, theta, z)
+	rVal, err := strconv.ParseFloat(r, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid r value"})
+		return
+	}
+	thetaVal, err := strconv.ParseFloat(theta, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid theta value"})
+		return
+	}
+	zVal, err := strconv.ParseFloat(z, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid z value"})
+		return
+	}
 	sr, stheta, sphi := polarToSpherical(rVal, thetaVal, zVal)
 
 	c.JSON(200, gin.H{"r": sr, "theta": stheta, "phi": sphi})
@@ -53,7 +111,21 @@ func SphericalToCartesianHandler(c *gin.Context) {
 	theta := c.Query("theta")
 	phi := c.Query("phi")
 
-	rVal, thetaVal, phiVal := convertToFloat(r, theta, phi)
+	rVal, err := strconv.ParseFloat(r, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid r value"})
+		return
+	}
+	thetaVal, err := strconv.ParseFloat(theta, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid theta value"})
+		return
+	}
+	phiVal, err := strconv.ParseFloat(phi, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid phi value"})
+		return
+	}
 	cx, cy, cz := sphericalToCartesian(rVal, thetaVal, phiVal)
 
 	c.JSON(200, gin.H{"x": cx, "y": cy, "z": cz})
@@ -64,7 +136,21 @@ func SphericalToPolarHandler(c *gin.Context) {
 	theta := c.Query("theta")
 	phi := c.Query("phi")
 
-	rVal, thetaVal, phiVal := convertToFloat(r, theta, phi)
+	rVal, err := strconv.ParseFloat(r, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid r value"})
+		return
+	}
+	thetaVal, err := strconv.ParseFloat(theta, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid theta value"})
+		return
+	}
+	phiVal, err := strconv.ParseFloat(phi, 64)
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid phi value"})
+		return
+	}
 	pr, ptheta, pz := sphericalToPolar(rVal, thetaVal, phiVal)
 
 	c.JSON(200, gin.H{"r": pr, "theta": ptheta, "z": pz})
